764519f32848c60e41e567489e367fbeb28b4c5b
Agustin Zalazar <agustin.zalazar@afforunity.com>
2023-05-25 21:28:40 -0300

    Changed redirect url

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -27,8 +27,8 @@ export default async function name(req: NextApiRequest, res: NextApiResponse) {
             const session = await stripe.checkout.sessions.create({
                 line_items: [...line_items],
                 mode: 'payment',
-                success_url: `http://localhost:3000/success`,
-                cancel_url: `http://localhost:3000/cancel`,
+                success_url: `${req.headers.origin}/success`,
+                cancel_url: `${req.headers.origin}/cancel`,
             });
             res.status(200).json({ session })
         } catch (error: any) {
